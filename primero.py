c7404472ed2ff3e7d5288ff6be918a89ac4c1ebd
= <=>
2023-05-23 21:00:32 -0500

    Primer cambio

---
+++ primero.py
@@ -0,0 +1,16 @@
+print('¿Cuál es tu nombre?')
+
+nombre = input()
+
+print('Hola ', nombre)
+
+print("Ingresa un númer a:")
+
+num1 = input()
+
+print("Ingrese un número b:")
+
+num2 = input()
+
+print(num1 + num2)
+
