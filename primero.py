f74ba16742f131980a0ec4a6be22e2e312a367b6
= <=>
2023-05-23 21:11:48 -0500

    Cambio 2

--- primero.py
+++ primero.py
@@ -6,11 +6,11 @@ print('Hola ', nombre)
 
 print("Ingresa un númer a:")
 
-num1 = input()
+num1 = input().isnumeric()
 
 print("Ingrese un número b:")
 
-num2 = input()
+num2 = input().isnumeric()
 
 print(num1 + num2)
 
